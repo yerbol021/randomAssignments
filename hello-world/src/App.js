3bea05b42f6fc4e4f8f746aa1778f88c7f56be2f
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-11 15:57:39 -0700

    Commit message

---
+++ hello-world/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <HelloWorld/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/components/HelloWorld.js
@@ -0,0 +1,8 @@
+function HelloWorld() {
+  return (
+    <h2>
+      Hello World!
+    </h2>  
+  );
+}
+export default HelloWorld;
