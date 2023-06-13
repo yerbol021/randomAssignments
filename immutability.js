952943d6625f29f3f8e076c633af92f0f293c735
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-12 19:26:01 -0700

    Learning how to properly manipulate objects and arrays

--- immutability.js
+++ immutability.js
@@ -18,3 +18,26 @@ const arrayOfNumbersNew = arrayOfNumbers.push(10);
 console.log("arrayOfNumbers: ", arrayOfNumbers);
 console.log("copyArrayOfNumbers: ",copyArrayOfNumbers);
 console.log("arrayOfNumbersNew: ", arrayOfNumbersNew);
+
+
+//Objects
+const object2 = {
+  key: "Value of Key",
+};
+
+const copyOfObject2 = { ...object2 };
+object2.key2 = "smth new";
+console.log("object2: ", object2);
+console.log("copyOfObject2: ", copyOfObject2);
+
+
+//Arrays
+const arrayOfNumbers2 = [1, 2, 3, 4, 5];
+
+const copyArrayOfNumbers2 = [...arrayOfNumbers2];
+
+arrayOfNumbers2.push(999);
+
+console.log("arrayOfNumbers2: ", arrayOfNumbers2);
+console.log("copyArrayOfNumbers2: ",copyArrayOfNumbers2);
+
