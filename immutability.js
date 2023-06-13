70e16e651c1267216e12c3aebae14369e3fd5ea7
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-12 19:17:45 -0700

    changing arrays and objects

--- immutability.js
+++ immutability.js
@@ -4,8 +4,17 @@ const object = {
 };
 
 const copyOfObject = object;
+object.key2 = "Value of New Key";
+console.log(object);
+console.log(copyOfObject);
 
 //Arrays
 const arrayOfNumbers = [1, 2, 3, 4, 5];
 
 const copyArrayOfNumbers = arrayOfNumbers;
+
+const arrayOfNumbersNew = arrayOfNumbers.push(10);
+
+console.log("arrayOfNumbers: ", arrayOfNumbers);
+console.log("copyArrayOfNumbers: ",copyArrayOfNumbers);
+console.log("arrayOfNumbersNew: ", arrayOfNumbersNew);
