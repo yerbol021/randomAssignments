fd41f314ee8a0c20fe19bba750ccd68a194e13f2
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-12 19:06:21 -0700

    next assignment

---
+++ immutability.js
@@ -0,0 +1,11 @@
+//Objects
+const object = {
+  key: "Value of Key",
+};
+
+const copyOfObject = object;
+
+//Arrays
+const arrayOfNumbers = [1, 2, 3, 4, 5];
+
+const copyArrayOfNumbers = arrayOfNumbers;
